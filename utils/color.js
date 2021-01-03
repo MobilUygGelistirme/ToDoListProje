9440862f2e1a2ccb132d36819ffe3ef0a6ea3c49
dgknmm <76878768+dgknmm@users.noreply.github.com>
2021-01-03 23:35:53 +0300

    Update color.js

--- utils/color.js
+++ utils/color.js
@@ -1,7 +1,9 @@
 const Color = {
     primary: '#0CB0D3',
     secondry: '#5FD9F3',
-    white: '#ffffff'
+    white: '#ffffff',
+    red: '#FF0000',
+    green: '#00FF00'
 }
 
 export default Color;
